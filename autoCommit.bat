3ef74bd5a1299f261289544176ca7f73a8770ae6
Norman Brumm <88732234+Norman-Brumm-IV@users.noreply.github.com>
2022-08-25 17:21:45 -0500

    08/25/2022_17:21:45.02

--- autoCommit.bat
+++ autoCommit.bat
@@ -1,4 +1,9 @@
-set theOneDate = date /t
+date /t|set theOneDate=
+echo %DATE%
+echo %TIME%
+set datetimef=%date:~4%_%time%
+echo %datetimef%
+
 git add .
-git commit -m theOneDate
+git commit -m %datetimef%
 git push
