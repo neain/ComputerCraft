59fec649ba8741acee03901a72fa119aa3389542
Norman Brumm <88732234+Norman-Brumm-IV@users.noreply.github.com>
2022-08-30 11:09:49 -0500

    08/30/2022

---
+++ shearSingleSheep.lua
@@ -0,0 +1,10 @@
+for i=1,200 do
+    turtle.place()
+    turtle.turnLeft()
+    turtle.turnLeft()
+    turtle.select(2)
+    turtle.drop()
+    turtle.turnLeft()
+    turtle.turnLeft()
+    os.sleep(300) -- 5 minutes
+end
